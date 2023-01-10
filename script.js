3a6a32478d4a952f6d055c1c3396386c108ee909
dorsariahi <dorsariahiasl@gmail.com>
2023-01-09 20:41:22 -0600

    if statement added

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ var forecastTitle = document.querySelector("#forecast");
 var forecastContainerEl = document.querySelector("#fiveday-container");
 var pastSearchButtonEl = document.querySelector("#past-search-buttons");
 
+var formSubmitHandler = function (event) {
+    event.preventDefault()
+    var city = cityInputEl.value.trim();
+    //if is true
+    if (city) {
+        getCityWeather(city);
+        get5Day(city);
+        cities.unshift({ city });
+        cityInputEl.value = "";
+    } else {
+        alert("Please enter a City");
+    }
+    saveSearch();
+    pastSearch(city);
+}
